421a50d41bfe1f836ad068229b684047682f818d
Raul Pineda <75541681+fernando5498@users.noreply.github.com>
2022-09-02 16:44:44 -0500

    page-silver

---
+++ assets/js/menu.js
@@ -0,0 +1,90 @@
+let headerTop = document.getElementById('header')
+
+function menuDown() {
+    if (window.scrollY >= 100) {
+        headerTop.classList.add('active')
+    } else {
+        headerTop.classList.remove('active')
+    }
+}
+menuDown()
+
+window.addEventListener('scroll', menuDown)
+
+/* -------------------- SCROLL SMOOTG ------------------- */
+
+let sections = document.querySelectorAll('section')
+let linkNav = document.querySelectorAll('.nav-r__link>a');
+
+linkNav.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        sections.forEach(section => {
+            if (section.id == button.dataset.link) {
+                position = section.offsetTop
+                window.scroll({
+                    top: position - 120,
+                    left: 0,
+                    behavior: 'smooth'
+                });
+            }
+        })
+    });
+});
+
+
+/* --------------------- MENU MOVILE -------------------- */
+
+let menuBurger = document.getElementById('menu-burger')
+let nav = document.getElementById('nav-r')
+let closeNav = document.getElementById('close-menu')
+
+
+
+menuBurger.addEventListener('click', () => {
+    nav.classList.toggle('active')
+})
+
+document.querySelector('main').addEventListener('click', () => {
+    nav.classList.remove('active')
+})
+
+linkNav.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('active')
+    })
+})
+
+
+
+
+closeNav.addEventListener('click', () => {
+    nav.classList.remove('active')
+})
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    dots: false,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        },
+        568: {
+            items: 2,
+
+        },
+        768: {
+            items: 3,
+            nav: false
+        },
+        1000: {
+            items: 4,
+            nav: true,
+            loop: false
+        }
+    }
+})
